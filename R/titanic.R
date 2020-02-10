ee6444c8e48ff255b53da28e85730871260e6ffd
Michel Lang <michellang@gmail.com>
2020-02-10 15:00:02 +0000

    version 0.1.0

---
+++ R/titanic.R
@@ -0,0 +1,24 @@
+#' @title Titanic
+#' @name titanic
+#'
+#' @description
+#' Classification data to predict the fate of passengers on the ocean liner "Titanic".
+#' Contains 10 features and 1309 observations. Target column is `"Survived"`.
+#'
+#' @section Pre-processing:
+#' * All column names have been changed to `snake_case`.
+#' * training and test set have been joined.
+#'   Observations of the test set have a missing value in the target column `"survived"`.
+#' * Id column has been removed.
+#' * Passenger class `"pclass"` has been converted to an ordered factor.
+#' * Features `"sex"` and `"embarked"` have been converted to factors.
+#' * Empty strings in `"cabin"` and `"embarked"` have been encoded as missing values.
+#'
+#' @source \CRANpkg{titanic} and \url{https://www.kaggle.com/c/titanic/data}
+#'
+#' @docType data
+#' @keywords data
+#' @examples
+#' data("titanic", package = "mlr3data")
+#' str(titanic)
+NULL
